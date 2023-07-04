8d7081212fdf73b6e84874031a3c0a117977bc8a
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-03 21:27:41 -0400

    justes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,12 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
+    <div className='md:flex md:min-h-screen'>
+       <div className='md:w-1/4 bg-blue-600 px-5 py-10'></div>
+       <div className="md:w-3/4 p-10 md:h-screen
+       
+       
+       "></div>
         <h1 className="text-6xl font-bold">CRM - React</h1>
         <Outlet />
     </div>
